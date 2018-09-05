b8e2cc30210d7b60210ec0b1b6c5904a4b98f317
farooqh <farooqhasny@gmail.com>
2018-09-05 21:40:26 +0800

    Update microbit.js

--- microbit.js
+++ microbit.js
@@ -47,7 +47,7 @@ function generateUint8Array() {
 
     for (let index = 0; index < 5; index++) {
       value *= 2;
-      if (document.getElementsByName("check" + row + index)[0].checked) {
+      if (document.getElementsByName("check" + row + index)[0].unchecked) {
         value += 1;
       }
     }
@@ -63,7 +63,7 @@ function requestDevice() {
   navigator.bluetooth.requestDevice({
     filters: [
       { services: [LED_SERVICE] },
-      { namePrefix: "BBC micro:bitt" }
+      { namePrefix: "BBC micro:bit" }
     ]
   })
     .then(device => {
